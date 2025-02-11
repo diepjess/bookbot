025d3828767818cebdfb86ff7b5daa94d4184bce
diepjess <jessicadiep97@gmail.com>
2025-02-10 19:23:48 -0500

    feat: Add function to get dictionary of character count of string

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words in the file")
+    num_characters = get_character_dict(text)
+    print(num_characters)
 
 
 def get_book_text(path):
@@ -29,6 +31,25 @@ def get_num_words(text):
     """
     words = text.split()
     return len(words)
-    
-    
+
+
+def get_character_dict(text):
+    """Get a dictionary of the character count in text
+
+    Args:
+        text (string): text of book as a single string
+
+    Returns:
+        dict: Keys are chars (including space and symbols) and value is their count
+    """
+    character_count = {}
+    lowered_text = text.lower()
+    for char in lowered_text:
+        if char in character_count:
+            character_count[char] += 1
+        else:
+            character_count[char] = 1
+    return character_count
+
+
 main()
